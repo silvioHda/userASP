78a1889ce5f4f565ad8c074840606303cab51393
silvio <silvioignacio_01@hotmail.com>
2019-12-22 16:17:24 -0600

    data annotations

--- UserASP/UserASP/Models/Cliente.cs
+++ UserASP/UserASP/Models/Cliente.cs
@@ -3,14 +3,23 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Data.Entity;
+using System.ComponentModel.DataAnnotations;
 
 namespace UserASP.Models
 {
     public class Cliente
     {
         public int ID { get; set; }
+        
+        [StringLength (60,MinimumLength = 3)]
         public string nombre { get; set; }
+
+        [Display (Name = "Fecha de Alta")]
+        [DataType (DataType.Date)]
+        [DisplayFormat (DataFormatString = "{0:YYYY-MM-dd}",ApplyFormatInEditMode = true)]
         public DateTime fechaAlta { get; set; }
+
+        [Range (18,75)]
         public int edad { get; set; }
 
     }
